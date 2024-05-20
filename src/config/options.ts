64b0291bfdeccbaf2493b6a4e1c53ac4738a2783
abuthahir-19 <abuthahirstar1@gmail.com>
2024-05-20 20:22:41 +0530

    BlogPost API Call

--- src/config/options.ts
+++ src/config/options.ts
@@ -1,23 +1,23 @@
-// const options = {
-//     method: 'GET',
-//     url: 'https://blogsapi.p.rapidapi.com/',
-//     params: {
-//         ordering: '-date_published'
-//     },
-//     headers: {
-//         'X-RapidAPI-Key': 'c98f2fc750mshcba7238edbcaa27p1fa8dajsn4ddc5c3c50b0',
-//         'X-RapidAPI-Host': 'blogsapi.p.rapidapi.com'
-//     }
-// };
-
 const options = {
     method: 'GET',
     url: 'https://blogsapi.p.rapidapi.com/',
-    params: { category: '5' },
+    params: {
+        ordering: '-date_published'
+    },
     headers: {
         'X-RapidAPI-Key': 'c98f2fc750mshcba7238edbcaa27p1fa8dajsn4ddc5c3c50b0',
         'X-RapidAPI-Host': 'blogsapi.p.rapidapi.com'
     }
 };
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://blogsapi.p.rapidapi.com/',
+//     params: { category: '5' },
+//     headers: {
+//         'X-RapidAPI-Key': 'c98f2fc750mshcba7238edbcaa27p1fa8dajsn4ddc5c3c50b0',
+//         'X-RapidAPI-Host': 'blogsapi.p.rapidapi.com'
+//     }
+// };
 
 export { options };
