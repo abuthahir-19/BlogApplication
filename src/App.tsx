4e209e0e191f83cce234d1930e188f3d8aeae558
abuthahir-19 <abuthahirstar1@gmail.com>
2024-05-20 20:16:37 +0530

    BlogPost API Call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from 'react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import { options } from './config/options';
+import { useTheme } from './hooks/useTheme';
+import axios from 'axios';
 
 function App() {
+    const { posts, setPosts } = useTheme();
+
+    useEffect (() => {
+        async function fetchPosts () {
+            try {
+                const response = await axios.request (options);
+                const data = response.data;
+                const results = data.results;
+                console.log (results);
+                
+                setPosts (results);
+            } catch (error) {
+                console.error (error);
+            }
+        }
+
+        fetchPosts();
+    }, [posts]);
+
     return (
         <div className="w-11/12 mx-auto">
             <Navbar />
--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,5 +1,30 @@
 import { type ReactNode, createContext, useState } from "react";
 
+type Category = {
+    id: number;
+    title: string;
+    categoryDesc: string;
+    categoryImage: string;
+};
+
+type Post = {
+    id: number;
+    category: Category;
+    title: string;
+    body: string;
+    excerpt: string;
+    tags: string;
+    contentSourceName: (string | null);
+    contentSourceURL: (string | null);
+    extraImageUrl: (string | null);
+    imageOwnerName: string;
+    imageOwnerBacklink: string;
+    image: string;
+    videoURL: (string | null);
+    date_published: string;
+    website: (string | null);
+}
+
 type AppContextProps = {
     children: ReactNode
 }
@@ -7,21 +32,26 @@ type AppContextProps = {
 type AppContextType = {
     isDarkMode: boolean;
     changeTheme: () => void;
+    posts: Post[];
+    setPosts: React.Dispatch<React.SetStateAction<Post[]>>
 }
 
 export const AppContext = createContext<AppContextType | null> (null);
 
 const AppContextProvider = ({ children }: AppContextProps) => {
     const [isDarkMode, setIsDarkMode] = useState (false);
+    const [posts, setPosts] = useState<Post[]>([]);
 
     const changeTheme = () => {
         setIsDarkMode (prevState => !prevState);
         // console.log (isDarkMode);
     }
-
+    
     const contextValue: AppContextType = {
         isDarkMode,
-        changeTheme
+        changeTheme,
+        posts,
+        setPosts,
     }
 
     return (
