3e65ae84fa2b98d0d193fc027906b6d1ceac24bd
abuthahir-19 <abuthahirstar1@gmail.com>
2024-06-04 21:24:33 +0530

    updated Post Page

--- src/pages/SinglePost.tsx
+++ src/pages/SinglePost.tsx
@@ -35,10 +35,10 @@ const SinglePost = () => {
             <div className="w-[85%] mx-auto x-ultra-xs:w-full">
                 <Navbar />
                 <ResponsiveNav />
-                <section className="w-2/3 mx-auto x-ultra-xs:w-[90%] ultra-xs:w-[95%]">
-                    <p className="bg-blue-500 px-[10px] py-[3px] inline-block text-white rounded-md font-medium text-medium mt-4 x-ultra-xs:text-sm ultra-xs:text-sm">{currBlog?.category.title}</p>
-                    <h1 className="text-4xl x-ultra-xs:text-lg ultra-xs:text-lg font-bold text-slate-800 mt-4 dark:text-white">{currBlog?.title}</h1>
-                    <div className="flex justify-between items-center py-3 w-[45%] x-ultra-xs:w-full ultra-xs:w-full mt-4">
+                <section className="w-2/3 mx-auto x-ultra-xs:w-[90%] ultra-xs:w-[95%] xxs:w-[95%] xs:w-[95%] sm:w-[95%] md:w-[90%]">
+                    <p className="bg-blue-500 px-[10px] py-[3px] inline-block text-white rounded-md font-medium text-medium mt-4 x-ultra-xs:text-sm ultra-xs:text-sm xxs:text-sm xs:text-sm sm:text-sm">{currBlog?.category.title}</p>
+                    <h1 className="text-4xl x-ultra-xs:text-lg ultra-xs:text-xl xxs:text-2xl xs:text-2xl sm:text-3xl font-bold text-slate-800 mt-4 dark:text-white">{currBlog?.title}</h1>
+                    <div className="flex justify-between items-center py-3 w-[55%] x-ultra-xs:w-full ultra-xs:w-full xxs:w-full xs:w-3/4 sm:w-3/4 mt-4">
                         <Link to={`/author/${currBlog?.author.id}/${currBlog?.author.name}`}>
                             <div className="flex justify-between items-center">
                                 {currBlog?.author.profile ? (
@@ -46,10 +46,10 @@ const SinglePost = () => {
                                 ) : (
                                     <FaUserCircle className="w-7 h-7 mr-2" />
                                 )}
-                                <p className="font-semibold text-slate-400 x-ultra-xs:text-sm ultra-xs:text-sm">{currBlog?.author.name}</p>
+                                <p className="font-semibold text-slate-400 x-ultra-xs:text-sm ultra-xs:text-sm xxs:text-base">{currBlog?.author.name}</p>
                             </div>
                         </Link>
-                        <p className="font-normal text-slate-400 x-ultra-xs:text-sm ultra-xs:text-sm">{getDateInfo(currBlog?.date_published as string)}</p>
+                        <p className="font-normal text-slate-400 x-ultra-xs:text-sm ultra-xs:text-sm xxs:text-base">{getDateInfo(currBlog?.date_published as string)}</p>
                     </div>
 
                     <img
@@ -58,14 +58,14 @@ const SinglePost = () => {
                         className="mt-4 rounded-xl w-full bg-center"
                     />
 
-                    <p className="text-xl x-ultra-xs:text-lg ultra-xs:text-lg mt-8 leading-[38px]">{currBlog?.body}</p>
+                    <p className="text-xl x-ultra-xs:text-lg ultra-xs:text-lg xxs:text-lg mt-8 leading-[38px]">{currBlog?.body}</p>
                     <img 
                         src={ currBlog?.category.categoryImage }
                         alt="Category" 
                         className="mt-4 rounded-xl w-full bg-center"
                     />
                     <div id="excerpt" className="p-5 mt-6 rounded-xl bg-slate-100 shadow-white dark:bg-gray-800">
-                        <p className="italic text-xl x-ultra-xs:text-lg ultra-xs:text-lg leading-[38px]">{` "${currBlog?.excerpt}" `}</p>
+                        <p className="italic text-xl x-ultra-xs:text-lg ultra-xs:text-lg xxs:text-lg leading-[38px]">{` "${currBlog?.excerpt}" `}</p>
                     </div>
                                 
                 </section>
